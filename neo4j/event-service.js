8a01847a0509732add389e37ca0bae67c3298708
adamdavenportGov <adam.davenport@nih.gov>
2024-02-15 15:12:10 -0500

    Revert "Added event handling for logining in and loggign out"
    This reverts commit 330e56ba600165d5023aa6f3459d98168729eea6.

--- neo4j/event-service.js
+++ neo4j/event-service.js
@@ -1,44 +1,51 @@
 const {getUserID, logEvent} = require("../bento-event-logging/neo4j/neo4j-operations");
-const mySQLOps = require("./mySQL-operations.js");
-const config = require('../config');
+const {LoginEvent} = require("../bento-event-logging/model/login-event");
+const {LogoutEvent} = require("../bento-event-logging/model/logout-event");
+const {LoginEventSQL} = require("../bento-event-logging/mySQL/LoginEvent.js");
 class EventService {
+
+
+
+
+
+
+
     constructor(databaseConnection) { //rename databse connection
         this.databaseConnection = databaseConnection.connection;
     } 
 
-    async storeLoginEvent(userID,userEmail, userIDP,databaseType){
 
-        if (databaseType == "NEO4J"){ //TODO make this toupper
-            let userID = await getUserID(this.neo4j, userEmail, userIDP);
-            if (userID === undefined){
-                userID = 'Not yet registered';
-            }
-            
-            const loginEvent = new LoginEvent(userID, userEmail, userIDP,connectionParams);
-            await logEvent(this.neo4j, loginEvent);
-            }
-        if (databaseType.toUpperCase() == "MYSQL"){
-            console.log("Switch to SQL ");
-            let eventType = "Login"
-            mySQLOps.getCreateCommand(userID,eventType,userEmail,userIDP);
+    async storeLoginEvent(userEmail, userIDP,databaseType){
+        if (databaseType = "NEO4J"){
+        let userID = await getUserID(this.neo4j, userEmail, userIDP);
+        if (userID === undefined){
+            userID = 'Not yet registered';
+        }
+        
+        const loginEvent = new LoginEvent(userID, userEmail, userIDP);
+        await logEvent(this.neo4j, loginEvent);
+        }
+        else if (databaseType = "MYSQL"){// convert to upper case for conparsion 
+        console.log("Switch to SQL ");
+        let LoginEventSQL = new LoginEventSQL(userID, this.mySQL)
+        this.databaseConnector.logEvent(LoginEventSQL);
        }
        else {
-            console.log("throw error");
+        console.log("throw error");
          }   
        
     }
 
-    async storeLogoutEvent(userID,userEmail, userIDP,databaseType){
-        
-        if (databaseType.toUpperCase() = "NEO4J"){
+    async storeLogoutEvent(userEmail, userIDP, databaseType){
+        if (databaseType = "NEO4J"){
         let userID = await getUserID(this.neo4j, userEmail, userIDP);
         const logoutEvent = new LogoutEvent(userID, userEmail, userIDP);
         await logEvent(this.neo4j, logoutEvent);
         }
-        if (databaseType.toUpperCase() == "MYSQL"){
+        else if (databaseType = "MYSQL"){
             console.log("Switch to SQL ");
-            let eventType = "Logout"
-            mySQLOps.getCreateCommand(userID,eventType,userEmail,userIDP);
+            let LoginEventSQL = new LoginEventSQL(userID, this.mySQL)
+            this.databaseConnector.logEvent(LoginEventSQL);
         }
         else {
             console.log("throw error");
