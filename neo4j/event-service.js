f73c4453c18eea1eb1655910704c402c96192a0b
adamdavenportGov <143554568+adamdaventryGov@users.noreply.github.com>
2024-12-20 11:17:23 -0500

    Revert "Merge branch 'Adam' into 1.1.0"
    This reverts commit 9c3312e0c8437120513d7726a9d364a370d21624, reversing
    changes made to 516d95fa5d8e3f8b9305151c1d75cdbeaa5947d1.

--- neo4j/event-service.js
+++ neo4j/event-service.js
@@ -1,52 +1,26 @@
 const {getUserID, logEvent} = require("../bento-event-logging/neo4j/neo4j-operations");
-const mySQLOps = require("../services/mySQL/mySQL-operations.js");
+const {LoginEvent} = require("../bento-event-logging/model/login-event");
+const {LogoutEvent} = require("../bento-event-logging/model/logout-event");
+
 class EventService {
-    constructor(databaseConnection) { //rename databse connection
-        this.databaseConnection = databaseConnection.connection;
-    } 
 
-    async storeLoginEvent(userID,userEmail, userIDP,databaseType){
-
-        if (databaseType.toUpperCase() == "NEO4J"){ //TODO make this toupper
-            let userID = await getUserID(this.neo4j, userEmail, userIDP);
-            if (userID === undefined){
-                userID = 'Not yet registered';
-            }
-            
-            const loginEvent = new LoginEvent(userID, userEmail, userIDP,connectionParams);
-            await logEvent(this.neo4j, loginEvent);
-            }
-        else if (databaseType.toUpperCase() == "MYSQL"){
-            console.log("Switch to SQL ");
-            let eventType = "Login";
-            
-
-            await mySQLOps.getCreateCommand(userID,eventType,userEmail,userIDP);
-            console.log("Has completed creating login event")
-       }
-       else {
-            throw new Error("Invalid database_type")
-         }   
-       
+    constructor(neo4j) {
+        this.neo4j = neo4j.connection;
     }
 
-    async storeLogoutEvent(userID,userEmail, userIDP,databaseType){
-        
-        if (databaseType.toUpperCase() == "NEO4J"){
-            let userID = await getUserID(this.neo4j, userEmail, userIDP);
-            const logoutEvent = new LogoutEvent(userID, userEmail, userIDP);
-            await logEvent(this.neo4j, logoutEvent);
+    async storeLoginEvent(userEmail, userIDP){
+        let userID = await getUserID(this.neo4j, userEmail, userIDP);
+        if (userID === undefined){
+            userID = 'Not yet registered';
         }
-        else if (databaseType.toUpperCase() == "MYSQL"){
-            let eventType = "Logout"
-            console.log("Switch to SQL ");
-            
-            await mySQLOps.getCreateCommand(userID,eventType,userEmail,userIDP);
-            console.log("Has completed creating logout event")
-        }
-        else {
-            throw new Error("Invalid database_type")
-        }
+        const loginEvent = new LoginEvent(userID, userEmail, userIDP);
+        await logEvent(this.neo4j, loginEvent);
+    }
+
+    async storeLogoutEvent(userEmail, userIDP){
+        let userID = await getUserID(this.neo4j, userEmail, userIDP);
+        const logoutEvent = new LogoutEvent(userID, userEmail, userIDP);
+        await logEvent(this.neo4j, logoutEvent);
     }
 }
 
