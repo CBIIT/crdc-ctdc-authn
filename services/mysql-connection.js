208871d4960e70d64c9c4e5a41274ee53e5d6f56
adamdavenportGov <143554568+adamdaventryGov@users.noreply.github.com>
2025-02-21 13:37:30 -0500

    Return code
    Try to reduce the number of returns in the code

--- services/mysql-connection.js
+++ services/mysql-connection.js
@@ -25,46 +25,35 @@ const getTTL = (req, res) => {
                 if (err){
                     console.log(err);
                     response = {ttl: null, error: "An error occurred while querying the database, see logs for details"};
-                    res.json(response);
-                    return;
                 }
                 else if (!rows || !rows[0] || !rows[0].expires){
                     response = {ttl: 0};
-                    res.json(response);
-                    return;
                 }
                 else{
                     let expires = rows[0].expires;
                     let dt = new Date(expires * 1000);
                     let ttl = Math.round((dt.valueOf() - Date.now())/1000);
                     response = {ttl: ttl};
-                    res.json(response);
-                    return;
                 }
-
-                
+                res.json(response);
+                currentConnection.release();
             });
         }
         else {
             res.json({ttl: null, error: "An internal server error occurred, please contact the administrators"});
-            return;
         }
         currentConnection.release();
     });
 }
 
 function getSessionIDFromCookie(req, res){
-    try{
     if (!req || !req?.cookies || !req?.cookies["connect.sid"]){
+        console.log("this Req " + req?.cookies)
         res.json({ttl: 0});
         return null;
     }
     else{
         return req?.cookies["connect.sid"].match(':.*[.]')[0].slice(1,-1);
-    }
-}
-    catch{
-        console.log("error getting session ID from cookie " + req)
     }
 }
 
